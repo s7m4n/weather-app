d24366dd842ad9b29271c9417b1c011598a2c2d5
Nelson Kenmochi <nelsonkenmochi@gmail.com>
2020-10-17 20:43:00 -0400

    Adiciona carregando + DRY

--- js/main.js
+++ js/main.js
@@ -2,43 +2,51 @@
 const key = "bfcae88cf13f3f2c05b4b4ebd3835341";
 
 // Funções
-function consultar(){
+function consultar() {
+    search(document.getElementById("cidade").value);
     $(".resultados").show(0); // Mostrar resultados na tela
-    const cidade = document.getElementById("cidade").value; // Valor inserido pelo usuário
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br`; // API
-    $.ajax({
-        url: url,
-        type: "GET", // request do tipo GET
-        success: function(result){
-            $("#nome").html(result.name);
-            $("#pais").html(result.sys.country);
-            $("#descricao").html(result.weather[0].description);
-            $("#vento").html(result.wind.speed);
-            $("#humidade").html(result.main.humidity);
-        }
-    })
 }
+
 /* Escolha feita pelo usuário com badges*/
-function cidades(cidade){
+function cidades(cidade) {
+    search(cidade);
+}
+
+// Função de procurar por cidades/países/etc
+function search(valueToSearch) {
+    const currentPage = $(".resultados").html();
+    $(".resultados").html("Carregando...");
     $(".resultados").show(0);
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${valueToSearch}&appid=${key}&lang=pt_br`;
     $.ajax({
         url: url,
         type: "GET",
-        success: function(result){
-            $("#nome").html(result.name);
-            $("#pais").html(result.sys.country);
-            $("#descricao").html(result.weather[0].description);
-            $("#vento").html(result.wind.speed);
-            $("#humidade").html(result.main.humidity);
-        }
-    })
+        success: (result) => {
+            $(".resultados").html(currentPage);
+            displayResultOnScreen(result);
+        },
+    }).fail((xhr, textStatus, errorThrown) => {
+        alert('Nenhum resultado encontrado.');
+        $(".resultados").html(currentPage);
+        $(".resultados").hide(0);
+    });
 }
+
+// Coloca informações na tela do usuário
+function displayResultOnScreen(result) {
+    $("#nome").html(result.name);
+    $("#pais").html(result.sys.country);
+    $("#descricao").html(result.weather[0].description);
+    $("#vento").html(result.wind.speed);
+    $("#humidade").html(result.main.humidity);
+}
+
 // esconder resultados e tela de carregamento
 $(() => {
     $(".resultados").hide(0);
 })
+
 // API
-function api(){
+function api() {
     window.open("https://api.openweathermap.org/");
 }
